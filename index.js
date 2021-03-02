c407e545caf0b23b3110e09aaaf57da0ac30a827
jshen212 <jeffshen@Jeffs-MacBook-Pro-2.local>
2021-03-01 16:45:18 -0800

    test

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
 app.get('/', (req, res) => res.send('GET / working'));
 
 app.listen(PORT, () => {
