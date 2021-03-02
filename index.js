d4f8dabb48c006de3e8edcc04536825022f4ee49
jshen212 <jeffshen@Jeffs-MacBook-Pro-2.local>
2021-03-01 16:51:18 -0800

    added db

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 const app = express();
 
-app.get('/', (req, res) => res.send('HELLO WORLD'));
+app
+    .get('/', (req, res) => res.send('HELLO WORLD'))
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * from test_table');
+            const results = { 'results': (result) ? result.rows : null };
+            res.send(results);
+        } catch (error) {
+            console.error(error);
+            res.send('Error getting /db: ', error);
+        } finally {
+            client.release();
+        }
+    });
 
 app.listen(PORT, () => {
   console.log(`Server running ${PORT}`);
