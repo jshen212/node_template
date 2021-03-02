31abde760d3946f8e62676c0c90709cba02c4812
jshen212 <jeffshen@Jeffs-MacBook-Pro-2.local>
2021-03-01 16:34:50 -0800

    fix server port

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +7,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(PORT, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
